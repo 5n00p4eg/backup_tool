54cb4b7591aad253b325f283d9a98fbec54b4fb5
Danya <5n00p4eg@gmail.com>
2013-01-04 14:56:07 +0200

    v

--- backup.sh
+++ backup.sh
@@ -1,6 +1,23 @@
 #!/bin/sh
 
+EXCLUDES=/home/snoopy/bin/backup.d/exclude.list
+BUACC="bijuws"
+BUDEST="/media/All-in/backup/$BUACC"
+BUDIR=`date +%M`
+BUOPTS="--force --ignore-errors --delete-excluded --exclude-from=$EXCLUDES 
+      --delete --backup --backup-dir=/$BUDIR -a"
+
 while read source
 do
-  echo $source
+  SOURCE="/$source"
+  DEST="$BUDEST/$BUDIR/$source"
+  CURRENT="$BUDEST/current/$source"
+  #Clean PERIOD destanation dir
+  rm -rf $DEST
+  #create target dirs
+  mkdir -p $DEST
+  mkdir -p $CURRENT
+
+  echo "$SOURCE > $DEST"
+  rsync $BUOPTS $SOURCE $CURRENT
 done < /home/snoopy/bin/backup.d/sources.list
