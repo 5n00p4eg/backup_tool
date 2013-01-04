d8b05b86e9dae6d473cb80f377d2b176ae6068d2
Danya <5n00p4eg@gmail.com>
2013-01-04 16:15:49 +0200

    Script improvements exclude updates

--- backup.sh
+++ backup.sh
@@ -6,18 +6,12 @@ BUDEST="/media/All-in/backup/$BUACC"
 BUDIR=`date +%Y-%m-%d`
 DEST="$BUDEST/$BUDIR"
 BUOPTS="--force --ignore-errors --delete-excluded --exclude-from=$EXCLUDES 
-      --delete --backup --backup-dir=$DEST -a -R --progress"
+      --delete --backup --backup-dir=$DEST -a -R"
 
 while read source
 do
   SOURCE="/$source"
   CURRENT="$BUDEST/current"
-  #Clean PERIOD destanation dir
-  rm -rf $DEST
-  #create target dirs
-  mkdir -p $DEST
-  mkdir -p $CURRENT
-
   echo "$SOURCE > $DEST"
   sudo rsync $BUOPTS $SOURCE $CURRENT
 done < /home/snoopy/bin/backup.d/sources.list
