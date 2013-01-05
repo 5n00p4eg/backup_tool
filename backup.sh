3b68f288f36988f3babbc1635520acd90fc70511
Boyko Daniil <5n00p4eg@gmail.com>
2013-01-05 11:41:58 +0200

    Update

--- backup.sh
+++ backup.sh
@@ -1,17 +1,21 @@
 #!/bin/sh
 
-EXCLUDES=/home/snoopy/bin/backup.d/exclude.list
-BUACC="bijuws"
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+. $SCRIPT_DIR/config.conf
+
+EXCLUDES="$SCRIPT_DIR/$SYSID/exclude.list"
+BUACC="$SYSID"
 BUDEST="/media/All-in/backup/$BUACC"
 BUDIR=`date +%Y-%m-%d`
 DEST="$BUDEST/$BUDIR"
 BUOPTS="--force --ignore-errors --delete-excluded --exclude-from=$EXCLUDES 
-      --delete --backup --backup-dir=$DEST -a -R"
+      --delete --backup --backup-dir=$DEST -a -R -v"
 
 while read source
 do
   SOURCE="/$source"
   CURRENT="$BUDEST/current"
   echo "$SOURCE > $DEST"
-  sudo rsync $BUOPTS $SOURCE $CURRENT
-done < /home/snoopy/bin/backup.d/sources.list
+  #sudo rsync $BUOPTS $SOURCE $CURRENT
+done < $SCRIPT_DIR/$SYSID/sources.list
